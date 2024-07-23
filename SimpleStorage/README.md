c8c60bd0200c56669dd08e879ad8a46fa4688369
harshana15 <harshana555prajapati@gmail.com>
2024-07-23 14:19:25 +0530

    Contracts added.

---
+++ SimpleStorage/README.md
@@ -0,0 +1,11 @@
+# Learning Foundry
+
+Getting into the nitty-gritty and usage of foundry. 
+
+Things I have learnt so far:
+
+```
+cast send <contractAddress> <functionsignature> <input to fxn> --account ACCOUNTNAME
+```
+> **Note:** > The last two arguments are required if you're using keystore.
+
---
+++ SimpleStorage/script/DeploySimpleStorage.s.sol
@@ -0,0 +1,17 @@
+//SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+import {SimpleStorage} from "../src/SimpleStorage.sol";
+
+contract DeploySimpleStorage is Script {
+    function run() external returns (SimpleStorage) {
+        vm.startBroadcast();
+        SimpleStorage simpleStorage = new SimpleStorage();
+        vm.stopBroadcast();
+        return simpleStorage;
+    }
+}
+
+//`crabby` keystore was saved successfully. Address: 0x976ea74026e726554db657fa54763abd0c3a0aa9
