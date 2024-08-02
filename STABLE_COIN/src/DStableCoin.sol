ef427a4894016844952186c4ced70face4d3d065
harshana15 <harshana555prajapati@gmail.com>
2024-08-02 16:44:32 +0530

    Stable coin implemented --- burn & mint fxns

---
+++ STABLE_COIN/src/DStableCoin.sol
@@ -0,0 +1,48 @@
+//SPDX-License-Identifier:MIT
+pragma solidity ^0.8.18;
+
+import {ERC20,ERC20Burnable} from "@openzepplin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+import {Ownable} from "@openzepplin/contracts/access/Ownable.sol";
+
+/**
+ * @title Decentralised Stable Coin
+ * @author Praharx
+ * Collateral: Exogenous (ETH & BTC)
+ * Minting: Algorithmic
+ * Relative Stability: Pegged to USD
+ * 
+ * This contract is governed by DSCEngine.This as ERC20 implementation of the stablecoin system.
+ */
+
+contract DStableCoin is ERC20Burnable,Ownable {
+    //errors
+    error DStableCoin__RevertZeroAddressReceiver();
+    error DStableCoin__AmountCantBeZero();
+    error DStableCoin__CantBurnZero();
+    error DStableCoin__BalanceInSufficeToBurnThisAmount();
+    constructor() ERC20("DStableCoin","CSD"){}
+
+    function burn(uint _amount) public override onlyOwner{
+        uint balance = balanceOf(msg.sender);
+
+        if (_amount <= 0){
+            revert DStableCoin__CantBurnZero();
+        }
+
+        if (balance < _amount){
+            revert DStableCoin__BalanceInSufficeToBurnThisAmount();
+        }
+        super.burn(_amount);
+    }
+
+    function mint(address _to, uint _amount) external onlyOwner returns (bool){
+        if(_to == address(0)){
+            revert DStableCoin__RevertZeroAddressReceiver();
+        }
+        if(_amount <= 0){
+            revert DStableCoin__AmountCantBeZero();
+        }
+        _mint(_to,_amount);
+        return true;
+    }
+}
